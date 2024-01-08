086f708b30f3e6eaee8f0f98706a84d5d361d543
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-08 20:59:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 34.79 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,17 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+    const participantMap = new Map();
     
-    for(let i = 0; i < participant.length; i += 1) {
-        if (participant[i] !== completion[i]) {
-            return participant[i];
+    participant.forEach(name => {
+        participantMap.set(name, (participantMap.get(name) || 0) + 1);
+    });
+    
+    completion.forEach(name => {
+        participantMap.set(name, participantMap.get(name) - 1);
+        
+        if(participantMap.get(name) === 0) {
+            participantMap.delete(name);
         }
-    }
+    });
+    
+    return [...participantMap.keys()][0]
 }
