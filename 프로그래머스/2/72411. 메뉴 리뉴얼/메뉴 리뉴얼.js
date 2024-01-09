e106d369a80c46a85fcf695e88686e08055e2928
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-09 21:44:04 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.57 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,64 @@
+function solution(orders, course) {
+    let result = [];
+  
+    course.forEach(count => {
+        const menuMap = new Map();
+        const combiArr = [];
+        let maxCount = 0;
+        
+        orders.forEach(order => {
+            combiArr.push(...combinations(order, count, 0, ''));
+        });
+        
+        countCombiMenus(combiArr, menuMap);
+        findMaxCombi(menuMap, result);
+    })
+    
+    return result.sort();
+}
+
+function findMaxCombi(menuMap, result) {
+    let maxCount = 0;
+    let maxCombis = [];
+    
+    menuMap.forEach((count, combi) => {
+        if (count >= 2 && count >= maxCount) {
+            if (count > maxCount) {
+                maxCombis.length = 0;
+            }
+            
+            maxCombis.push(combi);
+            maxCount = count;
+        }
+    });
+    
+    result.push(...maxCombis);
+}
+
+function countCombiMenus(arr, menuMap) {
+    for (const combi of arr) {
+        if (menuMap.has(combi)) {
+            menuMap.set(combi, menuMap.get(combi) + 1);
+        } else {
+            menuMap.set(combi, 1);
+        }
+    }
+    
+    return menuMap
+}
+
+function combinations(str, length, start = 0, current = '') {
+    const combis = [];
+    
+    if (length === 0) {
+        combis.push(current.split('').sort().join(''));
+        return combis;
+    }
+    
+    for (let i = start; i < str.length; i++) {
+        const newCurrent = current + str[i];
+        combis.push(...combinations(str, length -1, i + 1, newCurrent));
+    }
+    
+    return combis;
+}
