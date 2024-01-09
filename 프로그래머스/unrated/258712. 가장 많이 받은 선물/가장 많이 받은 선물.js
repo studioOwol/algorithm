c8189dae352f0790ac480b31e1591bbbd5b4b84c
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-09 14:59:30 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 11.58 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,54 @@
+function solution(friends, gifts) {
+    let giftRecords = {};
+    let giftIndex = {};
+    let result = 0;
+
+    for (let i = 0; i < friends.length; i++) {
+        giftRecords[friends[i]] = {};
+        giftIndex[friends[i]] = 0;
+
+        for (let j = 0; j < friends.length; j++) {
+            if (i !== j) {
+                giftRecords[friends[i]][friends[j]] = 0;
+            }
+        }
+    }
+    
+    gifts.forEach(gift => {
+        const [giver, taker] = gift.split(' ');
+        
+        giftRecords[giver][taker] += 1;
+        
+        giftIndex[giver] = (giftIndex[giver] || 0) + 1;
+        giftIndex[taker] = (giftIndex[taker] || 0) - 1;
+    })
+    
+    console.log(giftRecords)
+    console.log(giftIndex)
+    
+    friends.forEach(giver => {
+        let cnt = 0;
+        let giverIndex = giftIndex[giver];
+        
+        for (let taker in giftRecords[giver]) {
+            // 선물 준 개수가 받은 개수보다 많으면
+            if (giftRecords[giver][taker] > giftRecords[taker][giver]) {
+                cnt++;
+                continue;          
+            }
+           
+            if (giftRecords[giver][taker] === giftRecords[taker][giver]) {
+                // 선물지수가 높으면
+                if (giverIndex > giftIndex[taker]) {
+                    cnt++;
+                }
+            }
+        }
+                                
+        if (result <= cnt) {
+            result = cnt;
+        }
+    });
+    
+    return result;
+}
