48aafe340e8aef8ffcdbc98db8b69c8b853d0ab2
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-10 13:18:14 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.08 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,64 +1,29 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
 function solution(orders, course) {
-  const ordered = {};
-  const candidates = {};
-  const maxNum = Array(10 + 1).fill(0);
-  const createSet = (arr, start, len, foods) => {
-    if (len === 0) {
-      ordered[foods] = (ordered[foods] || 0) + 1;
-      if (ordered[foods] > 1) candidates[foods] = ordered[foods];
-      maxNum[foods.length] = Math.max(maxNum[foods.length], ordered[foods]);
-      return;
-    }
-
-    for (let i = start; i < arr.length; i++) {
-      createSet(arr, i + 1, len - 1, foods + arr[i]);
-    }
-  };
-
-  orders.forEach((od) => {
-    const sorted = od.split('').sort();
-    course.forEach((len) => {
-      createSet(sorted, 0, len, '');
+    const ordered = {};
+    const candidates = {};
+    const maxNum = Array(11).fill(0);
+    const createSet = (arr, start, len, menus) => {
+        if (len === 0) {
+            ordered[menus] = (ordered[menus] || 0) + 1;
+            
+            if (ordered[menus] > 1) candidates[menus] = ordered[menus];
+            maxNum[menus.length] = Math.max(maxNum[menus.length], ordered[menus]);
+            return;
+        }
+        
+        for (let i = start; i < arr.length; i += 1) {
+            createSet(arr, i + 1, len -1, menus + arr[i]);
+        }
+    };
+    
+    orders.forEach(order => {
+        const sorted = order.split('').sort();
+        course.forEach(len => {
+            createSet(sorted, 0, len, '');
+        });
     });
-  });
-
-  const launched = Object.keys(candidates).filter(
-    (food) => maxNum[food.length] === candidates[food]
-  );
-
-  return launched.sort();
+    
+    const result = Object.keys(candidates).filter(combi => maxNum[combi.length] === candidates[combi]);
+    
+    return result.sort();
 }
