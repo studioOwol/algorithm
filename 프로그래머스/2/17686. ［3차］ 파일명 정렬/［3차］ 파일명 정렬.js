015ff75641b518d2395908434624fe3daa7a3c60
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-10 17:06:07 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 18.14 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,14 +1,35 @@
 function solution(files) {
-    const sortedFiles = files.sort((a, b) => {
-        const regex = /\d+/;
-        const aNum = parseInt(a.match(regex)[0]);
-        const bNum = parseInt(b.match(regex)[0]);
+    let sortedFiles = files.map(file => {
+        let head = '';
+        let num = '';
+        let tail = '';
+        let flag = false;
 
-        const aHead = a.split(regex)[0].toLowerCase();
-        const bHead = b.split(regex)[0].toLowerCase();
+        for (let i = 0; i < file.length; i++) {
+            if (!isNaN(parseInt(file[i]))) {
+                num += file[i];
+                flag = true;
+            } else if (!flag) {
+                head += file[i];
+            } else {
+                tail = file.slice(i);
+                break;
+            }
+        }
 
-        return aHead.localeCompare(bHead) || aNum - bNum;
+        return { head, num, tail };
     });
 
-    return sortedFiles;
+    sortedFiles.sort((a, b) => {
+        const compareHead = a.head.toLowerCase().localeCompare(b.head.toLowerCase());
+        const compareNum = parseInt(a.num) - parseInt(b.num);
+
+        return compareHead || compareNum;
+    });
+
+    let result = sortedFiles.map(file => file.head + file.num + file.tail);
+    
+    console.log(result)
+
+    return result;
 }
