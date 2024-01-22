c6675965a0991a39da5b62df62faf6812b78f428
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-22 17:27:20 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,26 @@
+function solution(n, computers) {
+    let visited = Array(n).fill(false);
+    let answer = 0;
+    for (let i = 0; i < n; i ++) {
+        if (!visited[i]) {
+            bfs(i);
+            answer++;
+        }
+    }
+    
+    function bfs(node) {
+        let queue = [];
+        queue.push(computers[node]);
+        while (queue.length) {
+            let computer = queue.shift();
+            for (let i = 0; i < n; i++) {
+                if (computer[i] === 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.push(computers[i])
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
