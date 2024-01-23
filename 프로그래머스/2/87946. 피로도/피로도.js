70e812dbc2680bbd4669288fd844ab437102e7df
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-23 10:42:31 +0900

    [level 2] Title: 피로도, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,15 +1,15 @@
 function solution(k, dungeons) {
-    let visited = Array.from({length: dungeons.length}, () => false);
+    let visited = Array(dungeons.length).fill(false);
     let result = 0;
     
-    const dfs = (cnt, k) => {
+    function dfs(cnt, k) {
         result = Math.max(cnt, result);
-
+        
         for (let i = 0; i < dungeons.length; i++) {
             if (k >= dungeons[i][0] && !visited[i]) {
                 visited[i] = true;
                 dfs(cnt + 1, k - dungeons[i][1]);
-                visited[i] = false; 
+                visited[i] = false;
             }
         }
     }
