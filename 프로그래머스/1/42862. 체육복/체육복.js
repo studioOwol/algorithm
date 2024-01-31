cd3efa7c12250ce0c987bc3bce27c293c3eb5463
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-31 21:59:17 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,17 @@
+function solution(n, lost, reserve) {
+    let answer = [];
+    
+    const newLost = lost.filter(num => !reserve.includes(num)).sort((a, b) => a - b);
+    const newReserve = reserve.filter(num => !lost.includes(num)).sort((a, b) => a - b);
+    
+    newLost.forEach(lostNum => {
+        newReserve.forEach((reserveNum, idx) => {
+            if (Math.abs(lostNum - reserveNum) === 1) {
+                answer.push(lostNum);
+                newReserve.splice(idx, 1);
+            }
+        })
+    });
+    
+    return n - newLost.length + answer.length;
+}
