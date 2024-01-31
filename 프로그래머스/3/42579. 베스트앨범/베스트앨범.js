56477f9b1dccf0e3662b782a3814e4913eda2fd4
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-31 13:05:22 +0900

    [level 3] Title: 베스트앨범, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,43 +1,21 @@
 function solution(genres, plays) {
-    let genreCnt = {};
-    let songCnt = {};
-    let sortedSong = {};
-    let answer = [];
-    
-    genres.forEach((genre, idx) => {
-        if (!genreCnt[genre] || !songCnt[genre]) {
-            genreCnt[genre] = 0;
-            songCnt[genre] = [];
-        }
-        
-        genreCnt[genre] += plays[idx];
-        songCnt[genre].push({ [idx]: plays[idx]});
-    });
-    
-    let genreOrder = Object.keys(genreCnt).sort((a, b) => genreCnt[b] - genreCnt[a]);
+    const genreCnt = {};
+    const songCnt = {};
 
-    
-    const sortSongs = (a, b) => {
-        const aValue = Object.values(a)[0];
-        const bValue = Object.values(b)[0];
-        
-        if (aValue > bValue) return -1;
-        if (aValue < bValue) return 1;
-        
-        const aKey = Object.keys(a)[0];
-        const bKey = Object.keys(b)[0];
-        return aKey.localeCompare(bKey);
-    }
-    
-    for (const key in songCnt) {
-        sortedSong[key] = songCnt[key].sort(sortSongs).slice(0, 2);
-    }
-    
-    genreOrder.forEach(genre => {
-       sortedSong[genre].forEach(value => {
-           answer.push(Number(Object.keys(value)[0]));
-       });
+    genres.forEach((genre, idx) => {
+        genreCnt[genre] = (genreCnt[genre] || 0) + plays[idx];
+        songCnt[genre] = [...(songCnt[genre] || []), { index: idx, plays: plays[idx] }];
     });
+
+    const genreOrder = Object.keys(genreCnt).sort((a, b) => genreCnt[b] - genreCnt[a]);
+
+    const sortedSong = {};
+    
+    for (const genre in songCnt) {
+        sortedSong[genre] = songCnt[genre].sort((a, b) => b.plays - a.plays).slice(0, 2);
+    }
+    
+    const answer = genreOrder.reduce((answer, genre) => answer.concat(sortedSong[genre].map(item => item.index)), []);
     
     return answer;
 }
