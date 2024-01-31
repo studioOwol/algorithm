d9cfba988c654aa4832299580ab4c5891b4586b0
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-01-31 13:24:14 +0900

    [level 3] Title: 베스트앨범, Time: 3.40 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -6,13 +6,18 @@ function solution(genres, plays) {
         genreCnt[genre] = (genreCnt[genre] || 0) + plays[idx];
         songCnt[genre] = [...(songCnt[genre] || []), { index: idx, plays: plays[idx] }];
     });
+    
+    console.log(songCnt)
 
     const genreOrder = Object.keys(genreCnt).sort((a, b) => genreCnt[b] - genreCnt[a]);
 
     const result = [];
     
     for (const genre of genreOrder) {
-        const sortedSongs = songCnt[genre].sort((a, b) => b.plays - a.plays);
+        const sortedSongs = songCnt[genre].sort((a, b) => {
+            const aValue = b.plays - a.plays;
+            return aValue !== 0 ? aValue : a.index - b.index;
+        });
         result.push(...sortedSongs.slice(0, 2).map(song => song.index));
     }
 
