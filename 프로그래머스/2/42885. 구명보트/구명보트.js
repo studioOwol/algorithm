f8f3c2ecd7efc0991ed6bb15536bb92c488740a8
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-02-02 15:05:46 +0900

    [level 2] Title: 구명보트, Time: 10.52 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,10 +1,10 @@
 function solution(people, limit) {
-    let cnt = 0;
+    people.sort((a, b) => a - b);
+    
     let left = 0;
     let right = people.length - 1;
+    let cnt = 0;
     
-    people.sort((a, b) => a - b);
-
     while (left <= right) {
         cnt++;
         
