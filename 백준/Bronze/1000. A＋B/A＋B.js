18a19c5509d657ad599471651a7e1288480c6d4e
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-02-04 16:42:35 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
 
 const A = parseInt(input[0]);
 const B = parseInt(input[1]);
