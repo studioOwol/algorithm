7227152e8faec7329535bd59bdecd7239988c335
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-02-18 18:01:56 +0900

    [Silver I] Title: 회의실 배정, Time: 400 ms, Memory: 42872 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -8,14 +8,11 @@ const input = require('fs')
   .split('\n');
 
 let n = Number(input.shift());
-let meetings = input
-  .map((el) => el.split(' ').map(Number))
-  .sort((a, b) => {
-    if (a[1] === b[1]) {
-      return a[0] - b[0];
-    }
-    return a[1] - b[1];
-  });
+let meetings = input.map((el) => el.split(' ').map(Number));
+
+meetings.sort((a, b) => {
+  return a[1] === b[1] ? a[0] - b[0] : a[1] - b[1];
+});
 
 let end = meetings[0][1];
 let cnt = 1;
