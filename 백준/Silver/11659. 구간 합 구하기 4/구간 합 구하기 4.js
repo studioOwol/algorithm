2b749635d7d91eb528c04eb601238fb31602bbb8
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-02-24 14:13:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 392 ms, Memory: 55812 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -8,7 +8,7 @@ const inputs = require('fs')
 let [n, m] = inputs[0];
 let nums = inputs[1];
 let dp = Array(n + 1).fill(0);
-let answer = '';
+let answer = [];
 
 for (let i = 1; i < n + 1; i++) {
   dp[i] = dp[i - 1] + nums[i - 1];
@@ -17,7 +17,7 @@ for (let i = 1; i < n + 1; i++) {
 for (let i = 2; i < m + 2; i++) {
   let [start, end] = inputs[i];
   let sum = dp[end] - dp[start - 1];
-  answer += sum + '\n';
+  answer.push(sum);
 }
 
-console.log(answer);
+console.log(answer.join('\n'));
