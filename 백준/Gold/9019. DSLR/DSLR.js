ee54a5a09bb67bf3d00d0ce77b5d47928c3b92bd
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-03-01 20:02:40 +0900

    [Gold IV] Title: DSLR, Time: 13116 ms, Memory: 46160 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.js
@@ -0,0 +1,55 @@
+let [t, ...inputs] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let answer = [];
+let numbers = inputs.map((el) => el.split(' ').map(Number));
+
+const bfs = (a, b) => {
+  let visited = Array(10000).fill(false);
+  let queue = [[a, '']];
+  visited[a] = true;
+
+  while (queue.length) {
+    let [cur, commands] = queue.shift();
+
+    if (cur === b) {
+      answer.push(commands);
+      return;
+    }
+
+    let newD = (2 * cur) % 10000;
+    let newS = cur === 0 ? 9999 : cur - 1;
+    let newL = (cur % 1000) * 10 + Math.floor(cur / 1000);
+    let newR = (cur % 10) * 1000 + Math.floor(cur / 10);
+
+    if (!visited[newD]) {
+      visited[newD] = true;
+      queue.push([newD, commands + 'D']);
+    }
+
+    if (!visited[newS]) {
+      visited[newS] = true;
+      queue.push([newS, commands + 'S']);
+    }
+
+    if (!visited[newL]) {
+      visited[newL] = true;
+      queue.push([newL, commands + 'L']);
+    }
+
+    if (!visited[newR]) {
+      visited[newR] = true;
+      queue.push([newR, commands + 'R']);
+    }
+  }
+};
+
+for (let i = 0; i < Number(t); i++) {
+  let [a, b] = numbers[i];
+  bfs(a, b);
+}
+
+console.log(answer.join('\n'));
