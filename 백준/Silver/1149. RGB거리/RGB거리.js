2746d1822cc7403c0d7bbfd56312529444e6792c
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-03-04 20:57:52 +0900

    [Silver I] Title: RGB거리, Time: 128 ms, Memory: 9616 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -0,0 +1,16 @@
+let [n, ...inputs] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let costs = inputs.map((el) => el.split(' ').map(Number));
+let dp = costs;
+
+for (let i = 1; i < n; i++) {
+  dp[i][0] = dp[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+  dp[i][1] = dp[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+  dp[i][2] = dp[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+}
+
+console.log(Math.min(...dp[n - 1]));
