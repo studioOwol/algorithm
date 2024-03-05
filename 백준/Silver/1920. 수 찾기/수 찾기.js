35f532f40e29848b8c470e05f1810dd9b947e1be
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-03-05 16:01:06 +0900

    [Silver IV] Title: 수 찾기, Time: 312 ms, Memory: 46568 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -5,34 +5,12 @@ let inputs = require('fs')
   .split('\n')
   .map((el) => el.split(' ').map(Number));
 
-let [n] = inputs[0];
-let arr = inputs[1];
-let [m] = inputs[2];
+let arr = new Set(inputs[1]);
 let nums = inputs[3];
 let result = [];
 
-arr.sort((a, b) => a - b);
-
 for (let num of nums) {
-  let left = 0;
-  let right = n - 1;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-
-    if (arr[mid] === num) {
-      result.push(1);
-      break;
-    } else if (arr[mid] < num) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-
-  if (left > right) {
-    result.push(0);
-  }
+  result.push(arr.has(num) ? 1 : 0);
 }
 
 console.log(result.join('\n'));
