dbaa6a747e872f32599d00c03711cf13b332e4f2
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-03-08 13:12:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 204 ms, Memory: 14888 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -0,0 +1,29 @@
+let [t, ...inputs] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let [k, n] = t.split(' ').map(Number);
+let lans = inputs.map(Number);
+let left = 0;
+let right = Math.max(...lans);
+let result = 0;
+
+while (left <= right) {
+  let mid = Math.floor((left + right) / 2);
+  let cnt = 0;
+
+  for (let lan of lans) {
+    cnt += Math.floor(lan / mid);
+  }
+
+  if (cnt >= n) {
+    result = mid;
+    left = mid + 1;
+  } else {
+    right = mid - 1;
+  }
+}
+
+console.log(result);
