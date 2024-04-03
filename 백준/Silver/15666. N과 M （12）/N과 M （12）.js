e7ef85df36bd00e9cf46a137b87cbb2aaa3cc16c
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-04-03 14:06:24 +0900

    [Silver II] Title: N과 M (12), Time: 188 ms, Memory: 12116 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,28 @@
+let input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let [N, M] = input[0].split(' ').map(Number);
+let nums = [...new Set(input[1].split(' ').map(Number))];
+
+let answer = [];
+nums.sort((a, b) => a - b);
+
+makeSequence(0, []);
+
+console.log(answer.map((el) => el.join(' ')).join('\n'));
+
+function makeSequence(start, result) {
+  if (result.length === M) {
+    answer.push([...result]);
+    return;
+  }
+
+  for (let i = start; i < nums.length; i++) {
+    let newResult = [...result];
+    newResult.push(nums[i]);
+    makeSequence(i, newResult);
+  }
+}
