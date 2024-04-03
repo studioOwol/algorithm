70f088722818bc5c695b32545def2134ec2f6576
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-04-03 15:35:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+let [n, k] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+const jump = (n, k) => {
+  if (k <= n) {
+    return n - k;
+  }
+
+  if (k === 1) {
+    return 1;
+  }
+
+  if (k % 2 === 1) {
+    return 1 + Math.min(jump(n, k - 1), jump(n, k + 1));
+  }
+
+  if (k % 2 === 0) {
+    return Math.min(k - n, jump(n, k / 2));
+  }
+};
+
+console.log(jump(n, k));
