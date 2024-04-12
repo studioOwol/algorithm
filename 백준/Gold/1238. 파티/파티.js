12f6427de3eba90a80fad115fcaeebc2affc34c7
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-04-12 21:58:15 +0900

    [Gold III] Title: 파티, Time: 240 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.js
+++ 백준/Gold/1238. 파티/파티.js
@@ -7,114 +7,34 @@ let [t, ...input] = require('fs')
 let [N, M, X] = t.split(' ').map(Number);
 let linkedInfo = input.map((el) => el.split(' ').map(Number));
 let graph = Array.from({ length: N + 1 }, () => []);
-let result = [];
-let answer = Array(N + 1).fill(0);
+let revGraph = Array.from({ length: N + 1 }, () => []);
+let max = 0;
 
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  swap(a, b) {
-    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-  }
-
-  push(value, priority) {
-    this.heap.push([value, priority]);
-    this.bubbleUp();
-  }
-
-  pop() {
-    if (this.isEmpty()) {
-      return 0;
-    }
-
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
-
-    let minValue = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown();
-    return minValue;
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-
-    while (index > 0) {
-      let parentIdx = Math.floor((index - 1) / 2);
-
-      if (this.heap[parentIdx][1] > this.heap[index][1]) {
-        this.swap(index, parentIdx);
-        index = parentIdx;
-      } else {
-        break;
-      }
-    }
-  }
-
-  bubbleDown() {
-    let index = 0;
-
-    while (true) {
-      let leftIdx = index * 2 + 1;
-      let rightIdx = index * 2 + 2;
-      let smallerIdx = index;
-
-      if (
-        leftIdx < this.heap.length &&
-        this.heap[leftIdx][1] < this.heap[smallerIdx][1]
-      ) {
-        smallerIdx = leftIdx;
-      }
-
-      if (
-        rightIdx < this.heap.length &&
-        this.heap[rightIdx][1] < this.heap[smallerIdx][1]
-      ) {
-        smallerIdx = rightIdx;
-      }
-
-      if (smallerIdx !== index) {
-        this.swap(index, smallerIdx);
-
-        index = smallerIdx;
-      } else {
-        break;
-      }
-    }
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-}
-
+// 연결 정보 그래프 생성
 linkedInfo.forEach(([start, end, weight]) => {
   graph[start].push([end, weight]);
+  revGraph[end].push([start, weight]);
 });
 
-for (let i = 1; i <= N; i++) {
-  dijkstra(i);
-}
+// X 노드에서 각 노드로 가는 최단경로
+let dist = dijkstra(graph);
+// 각 노드에서 X 노드로 가는 최단경로
+let revDist = dijkstra(revGraph);
 
 for (let i = 1; i <= N; i++) {
-  if (i === X) continue;
-
-  answer[i] = result[X - 1][i] + result[i - 1][X];
+  let sum = dist[i] + revDist[i];
+  max = Math.max(max, sum);
 }
 
-console.log(Math.max(...answer));
+console.log(max);
 
-function dijkstra(start) {
+function dijkstra(graph) {
+  let queue = [[X, 0]];
   let dist = Array(N + 1).fill(Infinity);
-  let minHeap = new MinHeap();
-  minHeap.push(start, 0);
-  dist[start] = 0;
+  dist[X] = 0;
 
-  while (!minHeap.isEmpty()) {
-    let [cur, d] = minHeap.pop();
+  while (queue.length) {
+    let [cur, d] = queue.shift();
 
     if (dist[cur] < d) continue;
 
@@ -122,10 +42,10 @@ function dijkstra(start) {
       let cost = d + weight;
       if (cost < dist[node]) {
         dist[node] = cost;
-        minHeap.push(node, cost);
+        queue.push([node, dist[node]]);
       }
     }
   }
 
-  result.push(dist);
+  return dist;
 }
