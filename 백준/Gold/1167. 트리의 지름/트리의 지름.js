d40d325e2478bbe8dbba70932d9aa07a1d5519e8
studioOwol <116629752+studioOwol@users.noreply.github.com>
2024-04-13 21:33:39 +0900

    [Gold II] Title: 트리의 지름, Time: 632 ms, Memory: 93976 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.js
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.js
@@ -5,7 +5,9 @@ let [t, ...input] = require('fs')
   .split('\n');
 
 let N = Number(t);
-let graph = Array.from({ length: N + 1 }, () => []);
+let graph = Array.from({ length: N + 1 });
+graph[0] = [];
+let visited = Array(N + 1).fill(false);
 let result = 0;
 let maxNode = 0;
 
@@ -14,22 +16,26 @@ input.forEach((v) => {
   linkInfo.pop();
 
   for (let i = 0; i < linkInfo.length; i += 2) {
-    graph[start].push([linkInfo[i], linkInfo[i + 1]]);
+    graph[start] = linkInfo;
   }
 });
 
 dfs(1, 0, 0);
+visited.fill(0);
 dfs(maxNode, 0, 0);
 console.log(result);
 
 function dfs(node, parent, dist) {
+  visited[node] = true;
+
   if (dist > result) {
     result = dist;
     maxNode = node;
   }
 
-  for (let [next, cost] of graph[node]) {
-    if (next !== parent) {
+  for (let i = 0; i < graph[node].length - 1; i += 2) {
+    let [next, cost] = [graph[node][i], graph[node][i + 1]];
+    if (next !== parent && !visited[next]) {
       dfs(next, node, dist + cost);
     }
   }
