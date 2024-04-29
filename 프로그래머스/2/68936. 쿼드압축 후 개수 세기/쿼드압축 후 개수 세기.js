fb85bf28809b3ee3e77b16d425da28921f4bf9ba
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-04-29 20:43:28 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 6.31 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,31 +1,34 @@
 function solution(arr) {
     let cnt = [0, 0];
-    let n = arr.length;
+    let n = arr.length; 
     
     const recursion = (r, c, n) => {
-        let total = 0;
+        let num = arr[r][c];
+        let isSame = true;
         
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                total += arr[r + j][c + i];
+        for (let i = r; i < r + n; i++) {
+            for (let j = c; j < c + n; j++) {
+                if (arr[i][j] !== num) {
+                    isSame = false;
+                    break;
+                }
             }
         }
         
-        if (total === 0) {
-            cnt[0]++;
-        } else if (total === n * n) {
-            cnt[1]++;
-        } else {
-            n /= 2;
-            recursion(r, c, n);
-            recursion(r + n, c, n);
-            recursion(r, c + n, n);
-            recursion(r + n, c + n, n);
+        if (isSame) {
+            cnt[num]++;
+            return;
         }
+        
+        n /= 2;
+        recursion(r, c, n);
+        recursion(r + n, c, n);
+        recursion(r, c + n, n);
+        recursion(r + n, c + n, n);
     }
     
     recursion(0, 0, n);
-    
+
     return cnt;
 }
 
