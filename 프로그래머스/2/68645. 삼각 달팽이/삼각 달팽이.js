4d99b248ac61c037d3bcef305b0ffdbb0e078387
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-04-30 19:31:42 +0900

    [level 2] Title: 삼각 달팽이, Time: 110.26 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,7 +1,5 @@
 function solution(n) {
-    let result = [];
     let sum = (n * (n + 1)) / 2;
-    
     let arr = Array.from({length: n}, () => []);
     
     for (let i = 0; i < n; i++) {
@@ -9,32 +7,29 @@ function solution(n) {
     }
     
     const recursion = (r, limitR, c, number) => {
+        // 숫자가 블록의 숫자보다 커지면 종료(=블록이 다 채워지면)
         if (number > sum) return;
         
+        // 왼쪽으로 내려가면서 숫자를 채운다.
         for (let i = r; i < limitR; i++) {
             arr[i][c] = number++;
         }
         
+        // 맨 아랫층 숫자를 채운다.
         for (let i = c; i <= limitR - c; i++) {
             arr[limitR][i] = number++;
         }
         
+        // 오른쪽으로 올라가면서 숫자를 채운다.
         for (let i = limitR - 1; i > r; i--) {
             arr[i][i - c] = number++;
         }
         
+        // 시작층 + 2, 마지막층 - 1, 시작열 + 1
         recursion(r + 2, limitR - 1, c + 1, number);
     }
     
     recursion(0, n - 1, 0, 1);
     
-    let idx = 0;
-    
-    for (let i = 0; i < arr.length; i++) {
-        for (let num of arr[i]) {
-            result[idx++] = num;
-        }
-    }
-    
-    return result;
+    return arr.flat();
 }
