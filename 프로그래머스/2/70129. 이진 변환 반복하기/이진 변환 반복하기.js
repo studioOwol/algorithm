a011be26cb08ed14d10cd118cc549bc8f3a85f4c
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-01 20:18:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.62 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -2,15 +2,10 @@ function solution(s) {
     let binCnt = 0, zeroCnt = 0;
     
     while (s !== '1') {
-        for (let i = 0; i < s.length; i++) {
-            if (s[i] === '0') {
-                zeroCnt++;
-            }
-        }
-        
         binCnt++;
-        s = s.split('').filter(x => x !== '0').join('');
-        s = (s.length).toString(2);
+        let num = (s.match(/1/g) || []).length;
+        zeroCnt += s.length - num;
+        s = num.toString(2);
     }
     
     return [binCnt, zeroCnt];
