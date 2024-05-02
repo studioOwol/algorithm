cbbf8ac2860da19f5974e72e7a9e1f2ae14601f5
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-02 21:45:05 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,57 +1,16 @@
-function solution(s) {
-    let arr = s.split('');
-    let cnt = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        let temp = arr[0];
-        
-        for (let j = 0; j < s.length; j++) {
-            arr[j] = arr[j + 1];
-        }
-        arr[s.length - 1] = temp; 
-        
-        if (isRightParen(arr)) {
-            cnt++;
-        }
-    }
-    
-    return cnt;
-}
 
-function isRightParen(arr) {
-    let stack = [];
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (stack.length === 0) {
-            stack.push(arr[i])
-            continue;
-        }
-        
-        if (stack[stack.length - 1] === '(') {
-            if (arr[i] === ')') {
-                stack.pop();
-            } else {
-                stack.push(arr[i])
-            }
-        } else if (stack[stack.length - 1] === '{') {
-            if (arr[i] === '}') {
-                stack.pop();
-            } else {
-                stack.push(arr[i]);
-            }
-        } else if (stack[stack.length - 1] === '[') {
-            if (arr[i] === ']') {
-                stack.pop();
-            } else {
-                stack.push(arr[i]);
-            }
-        }
-        
+function solution(s) {
+  let answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    const arr = [];
+    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
+    for (let j = 0; j < temp.length; j++) {
+      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
+      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
+      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
+      else arr.push(temp[j]);
     }
-    
-    if (!stack.length) {
-        return true;
-    } else {
-        return false;
-    }
+    if (arr.length === 0) answer++;
+  }
+  return answer;
 }
