ff4307343b28e0de517110ece6defaccdee4c32e
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-22 22:43:19 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.13 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -1,15 +1,15 @@
 function solution(n, t, m, timetable) {
     let time = 540;
     
-    timetable = timetable.map(time => {
-        let [hour, min] = time.split(':').map(Number);
+    timetable = timetable.map(v => {
+        let [hour, min] = v.split(':').map(Number);
+        
         return hour * 60 + min;
     }).sort((a, b) => a - b);
     
     for (let i = 0; i < n; i++) {
         let krewLen = timetable.filter(v => v <= time).length;
         
-        // 마지막 버스인 경우
         if (i === n - 1) {
             if (krewLen >= m) {
                 time = timetable[m - 1] - 1;
@@ -20,7 +20,5 @@ function solution(n, t, m, timetable) {
         }
     }
     
-    return String(Math.floor(time / 60)).padStart(2, '0') + ':' + String(time % 60).padStart(2, '0')
+    return String(Math.floor(time / 60)).padStart(2, '0') + ':' + String(time % 60).padStart(2, '0');
 }
-
-
