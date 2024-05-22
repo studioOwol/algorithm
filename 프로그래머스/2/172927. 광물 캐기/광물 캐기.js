4790ddee6cdb9ea7afc17e8e7ef39ead3aff93dd
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-22 18:25:57 +0900

    [level 2] Title: 광물 캐기, Time: 0.27 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,8 +1,8 @@
 function solution(picks, minerals) {
-    let result = 0;
     let len = Math.ceil(minerals.length / 5);
-    let maxLen = picks.reduce((a, b) => a + b);
-    let arr = [];
+    let maxLen = picks.reduce((acc, v) => acc + v, 0);
+    let fatigue = [];
+    let result = 0;
     
     if (!maxLen) return 0;
     minerals = minerals.splice(0, maxLen * 5);
@@ -12,19 +12,33 @@ function solution(picks, minerals) {
         
         minerals.splice(0, 5).map(v => group[v[0]]++);
         
-        arr.push([
+        fatigue.push([
             group.d + group.i + group.s,
             group.d * 5 + group.i + group.s,
             group.d * 25 + group.i * 5 + group.s
         ]);
     }
     
-    arr
+    fatigue
         .sort((a, b) => b[2] - a[2])
         .map(v => {
-            if (picks[0] > 0) return picks[0]--, result += v[0];
-            if (picks[1] > 0) return picks[1]--, result += v[1];
-            if (picks[2] > 0) return picks[2]--, result += v[2];
+        if (picks[0] > 0) { // 다이아 곡괭이
+            picks[0]--;
+            result += v[0];
+            return
+        }
+        
+        if (picks[1] > 0) { // 철 곡괭이
+            picks[1]--;
+            result += v[1];
+            return
+        }
+        
+        if (picks[2] > 0) { // 돌 곡괭이
+            picks[2]--;
+            result += v[2];
+            return
+        }
     });
     
     return result;
