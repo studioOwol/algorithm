9e59b741d9993c39ea47f296c554f0e4a80a064d
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-22 20:06:03 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.12 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -1,52 +1,21 @@
 function solution(n, t, m, timetable) {
-    let time = 540;
-    
-    timetable = timetable.map(time => {
-        let [hour, min] = time.split(':').map(Number);
-        return hour * 60 + min;
-    }).sort((a, b) => a - b);
-    
+    const getTime = time => time.substr(0, 2) * 60 + +time.substr(3);
+
+    let answer = getTime('09:00'),
+        last = (n - 1) * t + answer,
+        crews = timetable.map(getTime).sort((a, b) => a - b).filter(v => v <= last);
+
     for (let i = 0; i < n; i++) {
-        let krewLen = timetable.filter(v => v <= time).length;
-        
-        // 마지막 버스인 경우
+        let crewsNum = crews.filter(crew => answer >= crew).length;
+
         if (i === n - 1) {
-            if (krewLen >= m) {
-                time = timetable[m - 1] - 1;
-            }
+            if (crewsNum >= m) answer = crews[m - 1] - 1;
         } else {
-            timetable.splice(0, krewLen > m ? m : krewLen);
-            time += t;
+            crews.splice(0, crewsNum > m ? m : crewsNum);
+
+            answer += t;
         }
     }
-    
-    return makeNumToTime(time);
-}
 
-function makeNumToTime(num) {
-    let hour = Math.floor(num / 60);
-    let min = num % 60;
-    
-    if (hour < 10 && min < 10) {
-        return `0${hour}:0${min}`;
-    }
-    
-    if (hour >= 10 && min < 10) {
-        return `${hour}:0${min}`;
-    }
-    
-    if (hour < 10 && min >= 10) {
-        return `0${hour}:${min}`;
-    }
-    
-    if (hour >= 10 && min >= 10) {
-        return `${hour}:${min}`;
-    }
+    return String(Math.floor(answer / 60)).padStart(2, '0') + ':' + String(answer % 60).padStart(2, '0');
 }
-
-// 셔틀은 09:00부터 총 n회 t분 간격으로 도착, 도착한 순간에 자리가 있다면 대기열에 선 크루도 탈 수 있음
-// 도착 시각 중 제일 늦은 시각
-// 같은 시각에 도착한 크루중 대기열 제일 뒤에 선다
-// 1 <= timetable <= 2000
-// 아이디어
-// 1. 시각을 분으로 환산하고 timetable 오름차순 정렬
