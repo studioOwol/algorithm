a445beaad2c2c66ab41df9bb12217772a43c2ea0
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-23 10:33:50 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.16 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -8,14 +8,20 @@ function solution(n, t, m, timetable) {
     }).sort((a, b) => a - b);
     
     for (let i = 0; i < n; i++) {
+        // 현재 버스 시간에 탈 수 있는 크루 인원
         let krewLen = timetable.filter(v => v <= time).length;
         
+        // 마지막 버스인 경우
         if (i === n - 1) {
+            // 최대 수용 인원 이상이면
             if (krewLen >= m) {
+                // 마지막으로 타는 크루보다 1분 일찍 도착해야 함
                 time = timetable[m - 1] - 1;
             }
         } else {
-            timetable.splice(0, krewLen > m ? m : krewLen);
+            // 현재 버스에 krewLen이 최대 수용 인원 m보다 크면 m명의 크루를 태우고, 작으면 크루 전원 태우기
+            timetable.splice(0, krewLen >= m ? m : krewLen);
+            // 다음 버스 출발 시간 업데이트
             time += t;
         }
     }
