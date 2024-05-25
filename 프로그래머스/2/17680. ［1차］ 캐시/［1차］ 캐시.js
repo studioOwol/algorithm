981f7a4fc1913d498231bb273869046addc6fd8a
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-05-25 15:57:33 +0900

    [level 2] Title: [1차] 캐시, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,29 +1,30 @@
 function solution(cacheSize, cities) {
     let cache = [];
-    let totalTime = 0;
-
+    let time = 0;
+    
     for (let city of cities) {
         city = city.toLowerCase();
         
         let idx = cache.indexOf(city);
-
+        
         if (idx !== -1) {
-            totalTime += 1;
+            // Cache hit
             cache.splice(idx, 1);
             cache.push(city);
+            time += 1;
         } else {
-            // Cache Miss: 실행 시간 5
-            totalTime += 5;
-            if (cache.length >= cacheSize && cacheSize > 0) {
-                // 캐시가 가득 차있으면 맨 앞의 도시를 제거
+            // Cache miss
+            if (cacheSize === cache.length && cacheSize > 0) {
                 cache.shift();
             }
             
             if (cacheSize > 0) {
                 cache.push(city);
             }
+            
+            time += 5;
         }
     }
-
-    return totalTime;
+    
+    return time;
 }
