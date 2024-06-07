6d1648541bd61232cf28aae343c869efb98209e0
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-06-07 16:48:14 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 397.95 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -2,14 +2,15 @@ function solution(numbers) {
     let answer = [];
     
     for (let num of numbers) {
+        if (num % 2 === 0) {
+            answer.push(num + 1);
+            continue;
+        }
         let binNum = ('0' + num.toString(2)).split('');
         let lastIdx = binNum.lastIndexOf('0');
         
         binNum[lastIdx] = '1';
-        
-        if (num % 2 === 1) {
-            binNum[lastIdx + 1] = '0';
-        }
+        binNum[lastIdx + 1] = '0';
         
         answer.push(parseInt(binNum.join(''), 2))
     };
