2952153695847c3028242cc25fa37c2c821da726
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2024-06-13 22:06:40 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,71 +1,41 @@
-let aMap = {};
-let bMap = {};
-let aList = [];
-let bList = [];
-let set;
-
 function solution(str1, str2) {
-    let a = str1.toLowerCase();
-    let b = str2.toLowerCase();
-    
-    for (let i = 0; i < a.length; i++) {
-        let str = a.slice(i, i + 2)
-        
-        if (/^[a-zA-Z]+$/.test(str) && str.length > 1) {
-            aList.push(str);
-          
-            if(!aMap[str]) {
-                aMap[str] = 0;
-            }
-            
-            aMap[str] += 1;
+     const bigrams1 = getBigrams(str1);
+    const bigrams2 = getBigrams(str2);
+
+    const map1 = new Map();
+    bigrams1.forEach(bigram => map1.set(bigram, (map1.get(bigram) || 0) + 1));
+
+    const map2 = new Map();
+    bigrams2.forEach(bigram => map2.set(bigram, (map2.get(bigram) || 0) + 1));
+
+    let intersectionSize = 0;
+    for (const [bigram, count] of map1.entries()) {
+        if (map2.has(bigram)) {
+            intersectionSize += Math.min(count, map2.get(bigram));
         }
     }
-    
-    for (let i = 0; i < b.length; i++) {
-        let str = b.slice(i, i + 2)
-        
-        if (/^[a-zA-Z]+$/.test(str) && str.length > 1) {
-            bList.push(str);
-            
-            if (!bMap[str]) {
-                bMap[str] = 0;
-            }
-        
-            bMap[str] += 1;
-        }
+
+    let unionSize = 0;
+    for (const [bigram, count] of map1.entries()) {
+        unionSize += count;
     }
-    
-    set = [...new Set([...aList, ...bList])];
-    
-    return getResult();
+    for (const [bigram, count] of map2.entries()) {
+        unionSize += count;
+    }
+    unionSize -= intersectionSize;
+
+    const similarity = unionSize === 0 ? 1 : intersectionSize / unionSize;
+    return Math.floor(similarity * 65536);
 }
 
-function getResult() {
-    let union = 0;
-    let intersec = 0;
-    
-    for (let str of set) {
-        let aCnt = 0;
-        let bCnt = 0;
-        
-        
-        if (aMap[str]) {
-            aCnt = aMap[str];
+function getBigrams(str) {
+    const bigrams = [];
+    const lowerStr = str.toLowerCase();
+    for (let i = 0; i < lowerStr.length - 1; i++) {
+        const bigram = lowerStr.slice(i, i + 2);
+        if (/^[a-z]{2}$/.test(bigram)) { 
+            bigrams.push(bigram);
         }
-        
-        if (bMap[str]) {
-            bCnt = bMap[str];
-        }
-        
-        intersec += Math.min(aCnt, bCnt);
-        union += Math.max(aCnt, bCnt);
     }
-    
-    if (!union) {
-        return 65536
-    }
-    
-    return Math.floor((intersec / union) * 65536)
+    return bigrams;
 }
-
