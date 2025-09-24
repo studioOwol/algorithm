493e0899bf2ae872ce4612f07dbbb278e8845e6f
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2025-09-24 15:18:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 24.56 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,51 +1,34 @@
-let R;
-let C;
-let visited;
-let ds = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-let answer = [];
-
 function solution(maps) {
-    R = maps.length;
-    C = maps[0].length;
-    
-    visited = Array.from({length: R}, () => Array(C).fill(false));
-    
-    for (let i = 0; i < R; i++) {
-        for (let j = 0; j < C; j++) {
-            if (!maps[i][j]) {
-                visited[i][j] = true;
-            }
-        }
-    }
-    
-    return bfs(maps);
-}
-
-function bfs(maps) {
+    let n = maps.length;
+    let m = maps[0].length;
+    let visited = Array.from({length: n}, () => Array(m).fill(false));
+    let ds = [[0, 1], [1, 0], [0, -1], [-1, 0]];
     let queue = [[0, 0, 1]];
+    
     visited[0][0] = true;
     
-    while (queue.length) {
-        let [r, c, cnt] = queue.shift();
-        
-        if (r === R - 1 && c === C - 1) {
-            return cnt;
-        }
-        
-        for (let d of ds) {
-            let nr = r + d[0];
-            let nc = c + d[1];
+    function bfs() {
+        while(queue.length) {
+            let [r, c, cnt] = queue.shift();
             
-            if (!(0 <= nr && nr < R && 0 <= nc && nc < C)) {
-                continue;
+            if (r === n - 1 && c === m - 1) {
+                return cnt;
             }
-
-            if (!visited[nr][nc]) {
-                visited[nr][nc] = true;
-                queue.push([nr, nc, cnt + 1]);
+            
+            for (let d of ds) {
+                let [nr, nc] = [r + d[0], c + d[1]];
+                
+                if (!(0 <= nr && nr < n && 0 <= nc && nc < m)) continue;
+                
+                if (!visited[nr][nc] && maps[nr][nc]) {
+                    visited[nr][nc] = true;
+                    queue.push([nr, nc, cnt + 1]);
+                }
             }
         }
+        
+        return -1;
     }
     
-    return -1;
+    return bfs();
 }
