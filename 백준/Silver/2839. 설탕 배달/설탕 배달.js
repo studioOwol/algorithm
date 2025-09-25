291d4cb7ea36c90211cf24888f3b463a9a91d049
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2025-09-25 15:53:38 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
-
-const input = +require('fs')
+let input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
   .toString()
   .trim();
 
-let sugar = Number(input);
-let cnt = 0;
+let N = Number(input);
+let dp = Array(N + 1).fill(-1);
 
-while (sugar >= 3) {
-  if (sugar % 5 === 0) {
-    sugar -= 5;
-    cnt++;
-  } else {
-    sugar -= 3;
-    cnt++;
+dp[3] = 1;
+if (N >= 5) dp[5] = 1;
+
+for (let i = 6; i < N + 1; i++) {
+  let case3 = dp[i - 3];
+  let case5 = dp[i - 5];
+
+  if (case3 !== -1 && case5 !== -1) {
+    dp[i] = Math.min(case3, case5) + 1;
+  } else if (case3 !== -1) {
+    dp[i] = case3 + 1;
+  } else if (case5 !== -1) {
+    dp[i] = case5 + 1;
   }
 }
 
-if (sugar === 0) {
-  console.log(cnt);
-} else {
-  console.log(-1);
-}
+console.log(dp[N]);
