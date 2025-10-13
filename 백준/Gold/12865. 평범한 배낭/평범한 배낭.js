2aecba97ddaef82c1d187d29f55bea80f37e331e
PARK NA HYUN <116629752+studioOwol@users.noreply.github.com>
2025-10-13 15:43:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 200 ms, Memory: 10476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,21 +1,20 @@
-let [t, ...input] = require('fs')
+let inputs = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
 
-let [n, m] = t.split(' ').map(Number);
-let table = input.map((v) => v.split(' ').map(Number));
-let dp = Array(m + 1).fill(0);
+const [N, K] = inputs.shift();
+const dp = new Array(K + 1).fill(0);
 
-for (let i = 0; i < n; i++) {
-  let [w, v] = [table[i][0], table[i][1]];
+for (let i = 0; i < N; i++) {
+  const w = inputs[i][0];
+  const v = inputs[i][1];
 
-  for (let j = m; j >= w; j--) {
-    if (w <= j) {
-      dp[j] = Math.max(dp[j], dp[j - w] + v);
-    }
+  for (let j = K; j >= w; j--) {
+    dp[j] = Math.max(dp[j], dp[j - w] + v);
   }
 }
 
-console.log(dp[m]);
+console.log(dp[K]);
